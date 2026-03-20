08bb00c8cfae7bf07b67ce2dce623367623a1eef
salvatore tullo <salvatoretullo15@gmail.com>
2026-03-20 12:54:42 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Homepage from './pages/Homepage'
+import Chisiamo from './pages/Chisiamo'
+import Prodotti from './pages/Prodotti'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout/>}>
+            <Route path="/" element={<Homepage/>}/>
+            <Route path="/chisiamo" element={<Chisiamo/>}/>
+            <Route path="/prodotti" element={<Prodotti/>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/layouts/DefaultLayout.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import AppHeader from "../components/AppHeader";
+import AppFooter from "../components/AppFooter";
+
+export default function DefaultLayout(){
+
+    return(
+        <>
+        <AppHeader/>
+        <Outlet/>
+        <AppFooter/>
+        </>
+    )
+}
