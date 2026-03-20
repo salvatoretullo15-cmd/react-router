6af778d2fec33bd4bf228d172661de9afbc9ff8d
salvatore tullo <salvatoretullo15@gmail.com>
2026-03-20 16:46:27 +0100

    implementazione di fetch e del ciclo map per la pagina prodotti

--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -1,7 +1,33 @@
+import { useState, useEffect } from "react";
+
 export default function Prodotti() {
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+            fetch('https://fakestoreapi.com/products')
+                .then(response => response.json())
+                .then(data => {
+                    setProducts(data); 
+                    console.log(data); 
+                })
+                .catch(error => console.error("Errore fetch:", error));
+    }, []);
     
     return(
         <>
+            <div className="container">
+                <h1>I nostri prodotti</h1>
+                <div className="products d-flex justify-content-center row">
+                    {products.map(product => (
+                        <div key={product.id} className="card col-md-2 m-3">
+                            <img src={product.image} alt={product.title} />
+                            <h2 className="fs-6">{product.title}</h2>
+                            <p className="">{product.price}$</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </>
     )
 }
