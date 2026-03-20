babf0c4f9722efcafd5d5b0e4970c36d8976a639
salvatore tullo <salvatoretullo15@gmail.com>
2026-03-20 17:46:58 +0100

    impostazione delle card con pochi successi

--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -18,14 +18,27 @@ export default function Prodotti() {
         <>
             <div className="container">
                 <h1>I nostri prodotti</h1>
-                <div className="products d-flex justify-content-center row">
-                    {products.map(product => (
-                        <div key={product.id} className="card col-md-2 m-3">
-                            <img src={product.image} alt={product.title} />
-                            <h2 className="fs-6">{product.title}</h2>
-                            <p className="">{product.price}$</p>
-                        </div>
-                    ))}
+                <div>
+                    <div className="row row-cols-1 row-cols-md-5 g-5">
+                        {products.map(product => (
+                            <div className="col" key={product.id}>
+                                <div className="card h-100">
+                                    <div className="img-card">
+                                        <img src={product.image} class="card-img-top" alt={product.text}/>
+                                    </div>
+                                    <div className="card-body">
+                                        <h5 className="card-title">{product.title}</h5>
+                                        <p className="card-text">{product.price}</p>
+                                    </div>
+                                    <div className="card-footer">
+                                        <small className="text-body-secondary">
+                                            Last updated 3 mins ago
+                                        </small>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </>
