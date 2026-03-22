5dc4c1a2c3841dc6afce133c6c442ad5a77bf611
salvatore tullo <salvatoretullo15@gmail.com>
2026-03-22 20:02:31 +0100

    implementazione della navbar e correzione delle card

--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -16,24 +16,22 @@ export default function Prodotti() {
     
     return(
         <>
-            <div className="container">
+            <div className="container-fluid row text-center">
                 <h1>I nostri prodotti</h1>
                 <div>
                     <div className="row row-cols-1 row-cols-md-5 g-5">
                         {products.map(product => (
                             <div className="col" key={product.id}>
-                                <div className="card h-100">
-                                    <div className="img-card">
-                                        <img src={product.image} class="card-img-top" alt={product.text}/>
+                                <div className="card h-100 align-items-center">
+                                    <div className="img-card h-50 mt-5">
+                                        <img src={product.image} class="card-img-top mb-5" alt={product.text}/>
                                     </div>
                                     <div className="card-body">
                                         <h5 className="card-title">{product.title}</h5>
-                                        <p className="card-text">{product.price}</p>
-                                    </div>
-                                    <div className="card-footer">
-                                        <small className="text-body-secondary">
-                                            Last updated 3 mins ago
-                                        </small>
+                                        <p className="card-text">prezzo: {product.price} $</p>
+                                        <button className="btn btn-primary">
+                                            acquista adesso
+                                        </button>
                                     </div>
                                 </div>
                             </div>
